2896b4d8fb5dff0fa879971eac8490cf63605fba
eleflea <dmzitnzc@gmail.com>
2024-05-24 18:29:11 +0800

    refactor profile

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,57 @@
+import time
+from dataclasses import dataclass
+from typing import Callable, Optional, Sequence, List
+
 import numpy as np
 import torch
-import time
 
 # from approx_adj_matrix import adj_matrix_numpy, adj_matrix_torch
 from adj_matrix import adj_matrix_numpy, adj_matrix_torch
+from profiler import Profiler
+
+
+_Tensor = torch.Tensor
+_ndarray = np.ndarray
+
+
+@dataclass
+class Case:
+    '''Class for benchmarking.'''
+    func: Callable
+    args: Sequence
+    times: int
+    warmup: int
+    name: Optional[str] = None
+    is_cuda: bool = False
+    primary: bool = False
+
+
+def to_numpy(t) -> _ndarray:
+    if isinstance(t, _ndarray):
+        return t
+    if isinstance(t, _Tensor):
+        return t.cpu().numpy()
+    raise TypeError(f'`{type(t)}` is neither `numpy.ndarray` nor `torch.Tensor`')
+
+
+def to_torch_cuda(t) -> _ndarray:
+    return torch.from_numpy(t).cuda()
+
+
+def compare_results(expect, output, detail=True):
+    expect = to_numpy(expect)
+    output = to_numpy(output)
+    all_close = np.allclose(expect, output)
+    if not all_close and detail:
+        abs_diff = np.abs(expect - output)
+        rel_diff = abs_diff / np.maximum(np.abs(expect), 1e-8)
+        print(f'max abs/rel error = {abs_diff.max()}/{rel_diff.max()}')
+        mask = rel_diff > 0.5
+        print('first 10 elements rel error > 0.5:')
+        print(f'expect: {expect[mask][:10].tolist()}')
+        print(f'output: {output[mask][:10].tolist()}')
+    return all_close
+
 
 def generate_inputs(segment_size, feature_size):
     segments = np.random.randint(feature_size[1], size=segment_size, dtype=np.int64)
@@ -22,28 +70,39 @@ def timeit(fn, args, times=100, cuda=False):
         torch.cuda.synchronize()
     return (time.time() - start) / times, fn(*args)
 
+
 def test():
+    b, h, w = 8, 224, 224
     n = 256
+    c = 128
     sigma = 5
-    segment_size = (8, 224, 224)
-    feature_size = (8, n, 128)
+    segment_size = (b, h, w)
+    feature_size = (b, n, c)
+
     segments, features = generate_inputs(segment_size, feature_size)
-    t_np, r_np = timeit(adj_matrix_numpy, (segments, features, sigma))
-    time.sleep(2)
-    segments_torch = torch.from_numpy(segments)
-    features_torch = torch.from_numpy(features)
-    segments_torch = segments_torch.cuda()
-    features_torch = features_torch.cuda()
-    t_torch_cuda, r_torch_cuda = timeit(adj_matrix_torch, (segments_torch, features_torch, sigma))
-    print(f'numpy time: {t_np: .6f}s\ntorch(cuda) time: {t_torch_cuda: .6f}s\nspeedup={t_np / t_torch_cuda: .3f}.')
-    r_torch_cuda = r_torch_cuda.cpu().numpy()
-    print(np.allclose(r_np, r_torch_cuda))
-    diff = np.abs(r_np - r_torch_cuda)
-    rel_diff = diff / np.maximum(np.abs(r_np), 1e-8)
-    print(f'max abs error: {diff.max()}; rel abs error: {rel_diff.max()}')
-    mask = rel_diff > 0.5
-    print(f'numpy: {r_np[mask][:10].tolist()}')
-    print(f'cuda: {r_torch_cuda[mask][:10].tolist()}')
+    args_np = (segments, features, sigma)
+    args_torch = (to_torch_cuda(segments), to_torch_cuda(features), sigma)
+
+    cases = [
+        Case(func=adj_matrix_numpy, args=args_np, times=100, warmup=25, primary=True),
+        Case(func=adj_matrix_torch, args=args_torch, times=200, warmup=25, is_cuda=True),
+    ]
+    profilers: List[Profiler] = []
+    expect_output = None
+    for case in cases:
+        p = Profiler(case.func, case.name)
+        print(f'running {p.name}...', end=' ', flush=True)
+        p.execute(case.args, case.times, case.warmup, case.is_cuda)
+        profilers.append(p)
+        if case.primary and expect_output is None:
+            expect_output = p.output
+        print('done', flush=True)
+    
+    for p in profilers:
+        p.summary()
+        acc_status = 'PASS' if compare_results(expect_output, p.output) else 'FAIL'
+        print(f'\tacc = {acc_status}')
+
 
 if __name__ == '__main__':
     test()
---
+++ profiler.py
@@ -0,0 +1,51 @@
+from typing import Callable, Optional, Sequence, Any, Dict
+import time
+
+import torch
+import numpy as np
+
+
+class Profiler:
+
+    def __init__(self, func: Callable, name: Optional[str] = None) -> None:
+        self.func = func
+        self.name = func.__name__ if name is None else name
+        self.output = None
+        self.records = []
+    
+    def execute(self, args: Sequence[Any], times: int, warmup: int = 0, is_cuda: bool = False):
+        if warmup == 0:
+            self.output = self.func(*args)
+        else:
+            for _ in range(warmup):
+                self.output = self.func(*args)
+
+        if is_cuda:
+            torch.cuda.synchronize()
+
+        for _ in range(times):
+            start = time.time()
+            self.func(*args)
+            if is_cuda:
+                torch.cuda.synchronize()
+            self.records.append(time.time() - start)
+        
+        return self
+
+    def statistic(self) -> Dict:
+        times = len(self.records)
+        stat = {}
+        stat['times'] = times
+        if times == 0:
+            return stat
+
+        stat['avg'] = np.mean(self.records).item() * 100
+        stat['min'] = np.min(self.records).item() * 100
+        stat['max'] = np.max(self.records).item() * 100
+        stat['std'] = np.std(self.records).item() * 100
+        stat['sum'] = np.sum(self.records).item() * 100
+        return stat
+
+    def summary(self) -> None:
+        stat = self.statistic()
+        print(f"'{self.name}' execute statistics:\n\tavg = {stat['avg']:.3f} ms, min/max/std = {stat['min']:.3f}/{stat['max']:.3f}/{stat['std']:.3f} ms")
